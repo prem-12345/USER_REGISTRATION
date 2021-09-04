e6bf76871e5f9b5661638eaca633ae68ad7b34f3
prem-12345 <premkpatil1997@gmail.com>
2021-09-04 14:16:51 +0530

    Welcome to Regular Expression program

---
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.userregistration;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+
+        // Welcome to Regular Expression Program
+
+    }
+
+}
---
+++ src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.userregistration;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRegistrationTest {
+
+
+}
