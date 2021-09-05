0c23a1ddf1360e25a025bb259e4ed62aa74c63a8
prem-12345 <premkpatil1997@gmail.com>
2021-09-05 17:16:37 +0530

    Added custom exception handling method

--- src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
@@ -5,69 +5,163 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UserRegistrationTest {
 
-    // UC1 Firstname validation test
+    // UC1 Firstname Custom Exception Handling
     @Test
     public void givenFirstname_When_Valid_ShouldReturn_True() {
         UserRegistration nameValidation = new UserRegistration();
-        boolean result = nameValidation.firstname("Prem");
-        assertTrue(result);
+        try {
+            boolean result = nameValidation.firstname("Prem");
+            if (result){
+                assertTrue(result);
+            }else {
+                throw new InvalidFirstNameTestException("Enter Correct First Name");
+            }
+
+        }catch (InvalidFirstNameTestException e) {
+            e.printStackTrace();
+        }
     }
 
-    // UC2 Lastname validation test
+    // UC2 Lastname Custom Exception Handling
     @Test
     public void givenLastname_When_Valid_ShouldReturn_True() {
         UserRegistration nameValidation = new UserRegistration();
-        boolean result = nameValidation.lastname("Patil");
-        assertTrue(result);
+        try {
+            boolean result = nameValidation.lastname("Patil");
+            if (result) {
+                assertTrue(result);
+            }else {
+                throw new InvalidLastNameTestException("Enter Correct Last Name");
+            }
+        }catch (InvalidLastNameTestException e){
+            e.printStackTrace();
+        }
     }
 
-    // UC3 Phone number validation test
+    // UC3 Phone number Custom Exception Handling
     @Test
     public void givenPhoneNumber_When_Valid_ShouldReturn_True() {
         UserRegistration phoneValidation = new UserRegistration();
-        boolean result = phoneValidation.phoneNumber("192 1580741236");
-        assertTrue((result));
+        try {
+            boolean result = phoneValidation.phoneNumber("192 1005841236");
+            if(result) {
+                assertTrue((result));
+            }else {
+                throw new InvalidPhoneNumberTestException("Enter The Correct Phone Number");
+            }
+        }catch (InvalidPhoneNumberTestException e){
+            e.printStackTrace();
+        }
     }
 
-    // UC4 Password validation test
+    // UC4 Password Custom Exception Handling
     @Test
     public void givenPassWord_When_Valid_ShouldReturn_True() {
         UserRegistration passWordValidation = new UserRegistration();
-        boolean result = passWordValidation.password("PREMPATIL");
-        assertTrue(result);
+        try {
+            boolean result = passWordValidation.password("PREMPATIL");
+            if (result) {
+                assertTrue(result);
+            }else {
+                throw new InvalidPassWordTestException("Enter Correct Password");
+            }
+        }catch (InvalidPassWordTestException e){
+            e.printStackTrace();
+        }
     }
 
-    // UC5 Uppercase Password validation test
+    // UC5 Uppercase Password Custom Exception Handling
     @Test
     public void givenUppercasePassWord_When_Valid_ShouldReturn_True() {
         UserRegistration passWordValidation = new UserRegistration();
-        boolean result = passWordValidation.uppercasePassword("pPremapu");
-        assertTrue(result);
+        try {
+            boolean result = passWordValidation.uppercasePassword("prePapuP");
+            if (result) {
+                assertTrue(result);
+            }else {
+                throw new InvalidPassWordTestException("Enter Correct Password");
+            }
+        }catch (InvalidPassWordTestException e){
+            e.printStackTrace();
+        }
     }
 
-    // UC6 Numeric Password validation test
+    // UC6 Numeric Password Custom Exception Handling
     @Test
     public void givenNumericPassWord_When_Valid_ShouldReturn_True() {
         UserRegistration passWordValidation = new UserRegistration();
-        boolean result = passWordValidation.numericPassword("p5RemPati");
-        assertTrue(result);
+        try {
+            boolean result = passWordValidation.numericPassword("p5RemPati");
+            if (result) {
+                assertTrue(result);
+            }else {
+                throw new InvalidPassWordTestException("Enter Correct Password");
+            }
+        }catch (InvalidPassWordTestException e){
+            e.printStackTrace();
+        }
     }
 
-    // UC7 Special Character Password validation test
+    // UC7 Special Character Password Custom Exception Handling
     @Test
     public void givenSpecialCharPassWord_When_Valid_ShouldReturn_True() {
         UserRegistration passWordValidation = new UserRegistration();
-        boolean result = passWordValidation.specialCharPassword("premp@6P");
-        assertTrue(result);
+        try {
+            boolean result = passWordValidation.specialCharPassword("premp@6P");
+            if(result) {
+                assertTrue(result);
+            }else {
+                throw new InvalidPassWordTestException("Enter Correct Password");
+            }
+        }catch (InvalidPassWordTestException e){
+            e.printStackTrace();
+        }
     }
 
-    // UC8 Email validation test
+    // UC8 Email Custom Exception Handling
     @Test
     public void givenEmail_When_Valid_ShouldReturn_True() {
         UserRegistration emailValidation = new UserRegistration();
-        boolean result = emailValidation.emailValid("prem123@gmail.com.");
-        assertTrue(result);
+        try {
+            boolean result = emailValidation.emailValid("prem123@gmail.com.");
+            if (result) {
+                assertTrue(result);
+            }else {
+                throw new InvalidEmailTestException("Enter The Correct Email Id");
+            }
+        }catch (InvalidEmailTestException e){
+            e.printStackTrace();
+        }
     }
 
 
+}
+class InvalidFirstNameTestException extends Exception {
+    InvalidFirstNameTestException(String message) {
+        super(message);
+    }
+}
+
+class InvalidLastNameTestException extends Exception {
+    InvalidLastNameTestException(String message) {
+        super(message);
+    }
+}
+
+class InvalidPhoneNumberTestException extends Exception {
+    InvalidPhoneNumberTestException(String message){
+        super(message);
+    }
+}
+
+class InvalidPassWordTestException extends Exception {
+    InvalidPassWordTestException (String message){
+        super(message);
+    }
+}
+
+class InvalidEmailTestException extends Exception {
+    InvalidEmailTestException (String message){
+        super(message);
+    }
 }
